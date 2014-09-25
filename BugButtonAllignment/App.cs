81f8db5a181de883b3a6c22586f63bc5c19e0187
Maik Switalski <Maik.Switalski@gmx.de>
2014-09-25 16:56:12 +0200

    initial commit.

---
+++ BugButtonAllignment/App.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace BugButtonAllignment
+{
+    public class App
+    {
+        public static Page GetMainPage()
+        {    
+            return new NavigationPage(new ContentPage { 
+                Content = new ScrollView {
+                    HeightRequest = 100,
+                    Content = new StackLayout {
+                        Children = {
+                            new StackLayout {
+                                Children = {
+                                    new Button {
+                                        Text = "b1",
+                                    },
+                                    new Button {
+                                        Text = "b2",
+                                    }
+                                }
+                            },
+                            //no tap highlighting !
+                            new Button {
+                                Text = "Hello World",
+                            },
+                        },
+                    },
+                },
+            });
+        }
+    }
+}
+
