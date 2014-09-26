3bbe5f5c3c7033a1d58978b33f9ca7dbfa6b366e
Maik Switalski <Maik.Switalski@gmx.de>
2014-09-26 10:57:42 +0200

    still broken.

--- BugButtonAllignment/App.cs
+++ BugButtonAllignment/App.cs
@@ -5,35 +5,24 @@ namespace BugButtonAllignment
 {
     public class App
     {
-        static ScrollView _scrollView;
-        static StackLayout _buttonsStack;
-
         public static Page GetMainPage()
         {   
-            _buttonsStack = new StackLayout();
-            _scrollView = new ScrollView {
-                Orientation = ScrollOrientation.Horizontal,
-                Content = _buttonsStack,
-            };
             Label l1 = new Label {
                 Text = "LabelText",
             };
 
-            _buttonsStack.Children.Add(l1);
-            _buttonsStack.Children.Add(new Button {
-                Text = "changeText",
-                Command = new Command(() => l1.Text += "1"),
-            });
-
             return new ContentPage { 
-                Content = _scrollView,
+                Content = new StackLayout {
+                    Children = {
+                        l1,
+                        new Button {
+                            Text = "changeText",
+                            Command = new Command(() => l1.Text += "1"),
+                        },
+                    }
+                }
             };
         }
-
-
-
-       
-
     }
 }
 
