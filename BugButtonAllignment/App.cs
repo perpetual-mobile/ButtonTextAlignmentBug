c5d4dfff4ad83ddcbb098983f14feddce7c7a7f3
Maik Switalski <Maik.Switalski@gmx.de>
2014-09-26 10:54:40 +0200

    broken.

--- BugButtonAllignment/App.cs
+++ BugButtonAllignment/App.cs
@@ -7,7 +7,6 @@ namespace BugButtonAllignment
     {
         static ScrollView _scrollView;
         static StackLayout _buttonsStack;
-        static Grid _buttonsGrid;
 
         public static Page GetMainPage()
         {   
@@ -15,18 +14,15 @@ namespace BugButtonAllignment
             _scrollView = new ScrollView {
                 Orientation = ScrollOrientation.Horizontal,
                 Content = _buttonsStack,
-                HeightRequest = 100
+            };
+            Label l1 = new Label {
+                Text = "LabelText",
             };
 
+            _buttonsStack.Children.Add(l1);
             _buttonsStack.Children.Add(new Button {
-                Text = "Add Control",
-                WidthRequest = 400
-            });
-            _buttonsStack.Children.Add(new Button {
-                Text = "Insert Control",
-            });
-            _buttonsStack.Children.Add(new Button {
-                Text = "Remove Control",
+                Text = "changeText",
+                Command = new Command(() => l1.Text += "1"),
             });
 
             return new ContentPage { 
@@ -34,35 +30,9 @@ namespace BugButtonAllignment
             };
         }
 
-        private static StackLayout ScrollStackLayout {
-            get { return (StackLayout)_scrollView.Content; }
-        }
 
-        private static void OnAddControl()
-        {
-            ScrollStackLayout.Children.Add(new Button { Text = "hello" });
-            ForceRelayout();
-        }
 
-        private static void OnInsertControl()
-        {
-            ScrollStackLayout.Children.Insert(0, new Button { Text = "hello" });
-            ForceRelayout();
-        }
-
-        private static void OnRemoveControl()
-        {
-            if (ScrollStackLayout.Children.Count > 0) {
-                ScrollStackLayout.Children.RemoveAt(0);
-                ForceRelayout();
-            }
-        }
-
-        private static void ForceRelayout()
-        {
-            ScrollStackLayout.ForceLayout();
-            _scrollView.ForceLayout();
-        }
+       
 
     }
 }
