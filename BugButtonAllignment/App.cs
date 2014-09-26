a9ae591e5221a6f477a31af20d51103ea51fdd54
Maik Switalski <Maik.Switalski@gmx.de>
2014-09-26 09:47:08 +0200

    still working

--- BugButtonAllignment/App.cs
+++ BugButtonAllignment/App.cs
@@ -5,32 +5,65 @@ namespace BugButtonAllignment
 {
     public class App
     {
+        static ScrollView _scrollView;
+        static StackLayout _buttonsStack;
+        static Grid _buttonsGrid;
+
         public static Page GetMainPage()
-        {    
-            return new NavigationPage(new ContentPage { 
-                Content = new ScrollView {
-                    HeightRequest = 100,
-                    Content = new StackLayout {
-                        Children = {
-                            new StackLayout {
-                                Children = {
-                                    new Button {
-                                        Text = "b1",
-                                    },
-                                    new Button {
-                                        Text = "b2",
-                                    }
-                                }
-                            },
-                            //no tap highlighting !
-                            new Button {
-                                Text = "Hello World",
-                            },
-                        },
-                    },
-                },
+        {   
+            _buttonsStack = new StackLayout();
+            _scrollView = new ScrollView {
+                Orientation = ScrollOrientation.Horizontal,
+                Content = _buttonsStack,
+                HeightRequest = 100
+            };
+
+            _buttonsStack.Children.Add(new Button {
+                Text = "Add Control",
+                WidthRequest = 400
             });
+            _buttonsStack.Children.Add(new Button {
+                Text = "Insert Control",
+            });
+            _buttonsStack.Children.Add(new Button {
+                Text = "Remove Control",
+            });
+
+            return new ContentPage { 
+                Content = _scrollView,
+            };
         }
+
+        private static StackLayout ScrollStackLayout {
+            get { return (StackLayout)_scrollView.Content; }
+        }
+
+        private static void OnAddControl()
+        {
+            ScrollStackLayout.Children.Add(new Button { Text = "hello" });
+            ForceRelayout();
+        }
+
+        private static void OnInsertControl()
+        {
+            ScrollStackLayout.Children.Insert(0, new Button { Text = "hello" });
+            ForceRelayout();
+        }
+
+        private static void OnRemoveControl()
+        {
+            if (ScrollStackLayout.Children.Count > 0) {
+                ScrollStackLayout.Children.RemoveAt(0);
+                ForceRelayout();
+            }
+        }
+
+        private static void ForceRelayout()
+        {
+            ScrollStackLayout.ForceLayout();
+            _scrollView.ForceLayout();
+        }
+
     }
 }
 
