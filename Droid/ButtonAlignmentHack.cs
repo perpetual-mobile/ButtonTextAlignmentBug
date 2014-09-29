07a2c4740eb36cb54428d95fc65f62749f4da004
Maik Switalski <Maik.Switalski@gmx.de>
2014-09-29 11:34:41 +0200

    renamed 'Android' to 'Droid'

---
+++ Droid/ButtonAlignmentHack.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using Droid;
+
+[assembly: ExportRenderer(typeof(Button), typeof(ButtonAlignmentHack))]
+namespace Droid
+{
+    public class ButtonAlignmentHack : ButtonRenderer
+    {
+        public override void ChildDrawableStateChanged(Android.Views.View child)
+        { 
+            base.ChildDrawableStateChanged(child); 
+            Control.Text = Control.Text; 
+        }
+    }
+}
+
